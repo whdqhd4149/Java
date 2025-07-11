051ac020c445f21ad6cd5bfadf09f41d6ac3275f
whdqhd4149 <whdqhd4149@gmail.com>
2025-07-11 11:35:38 +0900

    오버로드 메서드 수정

--- ch04/src/sub4/MethodOverloadTest.java
+++ ch04/src/sub4/MethodOverloadTest.java
@@ -14,11 +14,13 @@ public class MethodOverloadTest {
 	int r2 = add(2, 3);
 	double r3 = add(1.1, 3.1);
 	String r4 = add("Hello", "Java");
+	String r5 = add("number_", 1);
 	
 	System.out.println("r1 :" + r1);
 	System.out.println("r2 :" + r2);
 	System.out.println("r3 :" + r3);
 	System.out.println("r4 :" + r4);	
+	System.out.println("r5 :" + r5);	
 	}
 	
 	//오버로드 메서드 정의 : 메서드 일므은 같지만 매개변수로 구분되는 메서드
@@ -35,6 +37,11 @@ public class MethodOverloadTest {
 	}
 	
 	public static String add(String a, String b) {
+		return a + b;
+	}
+	
+	public static String add(String a, int b) {
+		
 		return a + b;
 	}
 
